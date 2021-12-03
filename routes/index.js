ecca533b235341d5a205ae418ea77d10d5055720
Kewal Mehta <mehtakewal25@stevens.edu>
2021-12-03 18:29:21 -0500

    Updated Code

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
+const userRoutes = require('./main');
 
+const constructorMethod = (app) => {
+app.use('/', userRoutes);
+
+app.use('*', (req, res) => {
+    res.sendStatus(404);
+});
+};
+
+module.exports = constructorMethod;
