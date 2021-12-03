4dcc6819b8427c1200ce1fc108f25f813584e886
kewal25 <95322909+kewal25@users.noreply.github.com>
2021-12-03 11:28:18 -0500

    Add files via upload
    Client Side Validation for Signup

---
+++ public/js/clientSideSignup.js
@@ -0,0 +1,204 @@
+(function () {
+
+    const myForm = document.getElementById("signup-form");
+    let usernameInput = document.getElementById("username");
+    let passwordInput = document.getElementById("password");
+    let firstNameInput = document.getElementById("firstname");
+    let lastNameInput = document.getElementById("lastname");
+    let phoneNumberInput = document.getElementById("phonenumber");
+    let errorDiv = document.getElementById('errorcheck');
+    if (myForm) {
+    myForm.addEventListener("submit", (event) => {
+        valid = true
+        if(!usernameInput.value){
+            event.preventDefault();
+            valid = false
+            usernameInput.value = ''
+            errorDiv.hidden = false;
+            errorDiv.innerHTML = 'Username must be provided'
+            return
+        }
+        else{
+            valid = true
+            errorDiv.hidden = true
+        }
+        if(!passwordInput.value){
+            event.preventDefault();
+            valid = false
+            passwordInput.value = ''
+            errorDiv.hidden = false
+            errorDiv.innerHTML = 'Password must be provided'
+            return
+        }else{
+            valid = true
+            errorDiv.hidden = true
+        }
+        if(!firstNameInput.value){
+            event.preventDefault();
+            valid = false
+            firstNameInput.value = ''
+            errorDiv.hidden = false
+            errorDiv.innerHTML = 'First Name must be provided'
+            return
+        }else{
+            valid = true
+            errorDiv.hidden = true
+        }
+        if(!lastNameInput.value){
+            event.preventDefault();
+            valid = false
+            lastNameInput.value = ''
+            errorDiv.hidden = false
+            errorDiv.innerHTML = 'Last Name must be provided'
+            return
+        }else{
+            valid = true
+            errorDiv.hidden = true
+        }
+        if(!phoneNumberInput.value){
+            event.preventDefault();
+            valid = false
+            phoneNumberInput.value = ''
+            errorDiv.hidden = false
+            errorDiv.innerHTML = 'Phone Number must be provided'
+            return
+        }else{
+            valid = true
+            errorDiv.hidden = true
+        }
+        if(usernameInput.value.length<4){
+            event.preventDefault();
+            valid=false
+            usernameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML = 'Length of the username should be atleast 4'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(passwordInput.value.length<6){
+            event.preventDefault();
+            valid=false
+            passwordInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML = 'Length of the password should be atleast 6'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(usernameInput.value.indexOf(' ')>=0){
+            event.preventDefault();
+            valid=false;
+            usernameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Username must not have empty spaces'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(passwordInput.value.indexOf(' ')>=0){
+            event.preventDefault();
+            valid=false
+            passwordInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Password cannot have empty spaces'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(firstNameInput.value.indexOf(' ')>=0){
+            event.preventDefault();
+            valid=false
+            firstNameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='First Name cannot have empty spaces'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(lastNameInput.value.indexOf(' ')>=0){
+            event.preventDefault();
+            valid=false
+            lastNameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Last Name cannot have empty spaces'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(phoneNumberInput.value.indexOf(' ')>=0){
+            event.preventDefault();
+            valid=false
+            phoneNumberInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Phone Number cannot have empty spaces'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        let regex4 = /[^0-9a-z]/gi;
+        if(usernameInput.value.match(regex4)){
+            event.preventDefault();
+            valid=false
+            usernameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Username cannot contain special characters'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(firstNameInput.value.match(regex4)){
+            event.preventDefault();
+            valid=false
+            firstNameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='First Name cannot contain special characters'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(lastNameInput.value.match(regex4)){
+            event.preventDefault();
+            valid=false
+            lastNameInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Last Name cannot contain special characters'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(phoneNumberInput.value.match(regex4)){
+            event.preventDefault();
+            valid=false
+            phoneNumberInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Phone Number cannot contain special characters'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+        if(phoneNumberInput.value.length != 10){
+            event.preventDefault();
+            valid=false
+            phoneNumberInput.value=''
+            errorDiv.hidden=false
+            errorDiv.innerHTML='Phone Number can only have 10 digits'
+            return
+        }else{
+            valid=true
+            errorDiv.hidden=true
+        }
+    });
+    }
+})();
