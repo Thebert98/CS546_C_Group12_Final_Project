b12be79b41e0380048dff2131928f4d6372f72b6
SeeAnish <amalhot6@stevens.edu>
2021-12-06 14:27:55 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const userRoutes = require('./main');
-
+const commentroutes = require('./comments');
 const constructorMethod = (app) => {
 app.use('/', userRoutes);
-
+app.use('/comments', commentroutes);
 app.use('*', (req, res) => {
     res.sendStatus(404);
 });
