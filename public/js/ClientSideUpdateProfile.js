e8cb16f8355863a1135e6a137da02a9f891532d0
Thebert98 <92902192+Thebert98@users.noreply.github.com>
2021-12-08 23:25:55 -0500

    bug fixes
    bug fixes

--- public/js/ClientSideUpdateProfile.js
+++ public/js/ClientSideUpdateProfile.js
@@ -2,25 +2,56 @@
 
     const myForm = document.getElementById("formForEditUser");
     let bioInput = document.getElementById("bio");
-    let favRecipeInput = document.getElementById("favouriteRecipe");
+    let favRecipeInput = document.getElementById("favoriteRecipe");
     let profilePictureInput = document.getElementById("profilePicture");
     let errorDiv = document.getElementById('errorcheck');
     if (myForm) {
     myForm.addEventListener("submit", (event) => {
         valid = true
-        if(!bioInput.value && !favRecipeInput && !profilePictureInput){
+        if( profilePictureInput.value){
+            if((profilePictureInput.value.substring(profilePictureInput.value.length -5 ).toLowerCase() !== ".jpeg")&&(profilePictureInput.value.substring(profilePictureInput.value.length -4 ).toLowerCase() !== ".jpg")&&(profilePictureInput.value.substring(profilePictureInput.value.length -4 ).toLowerCase() !== ".png")){
+                event.preventDefault();
+                valid = false
+                profilePictureInput.value=''
+                errorDiv.hidden = false;
+                errorDiv.innerHTML = 'An invalid file type was provided'
+                return
+            } 
+    
+        }
+        if(bioInput.value){
+            if(bioInput.value.length >= 500){
+                event.preventDefault();
+                valid = false
+                bio.value = ''
+                errorDiv.hidden = false;
+                errorDiv.innerHTML = 'Bio input is too long (500 character max)'
+                return
+            }
+        }
+        if(favRecipeInput.value){
+            if(favRecipeInput.value.length >20){
+                event.preventDefault();
+                valid = false
+                favRecipeInput.value = ''
+                errorDiv.hidden = false;
+                errorDiv.innerHTML = 'Favorite Recipe input is too long (20 character max)'
+                return
+            }
+        }
+        if(bioInput.value || favRecipeInput.value || profilePictureInput.value){
+            valid = true
+            errorDiv.hidden = true
+        }
+        else{
             event.preventDefault();
             valid = false
             bioInput.value = ''
-            favRecipeInput = ''
-            profilePictureInput=''
+            favRecipeInput.value = ''
+            profilePictureInput.value=''
             errorDiv.hidden = false;
             errorDiv.innerHTML = 'No Updates were provided'
             return
-        }
-        else{
-            valid = true
-            errorDiv.hidden = true
         }
     });
     }
