f36e511750aca6a175e0f410ce8d44711c82cd3f
Thebert98 <92902192+Thebert98@users.noreply.github.com>
2021-12-10 15:41:12 -0500

    minor update

--- data/index.js
+++ data/index.js
@@ -1,10 +1,12 @@
 const recipeData = require('./recipes');
 const commentsdata = require('./comments');
 const userdata = require('./users');
+const mainData = require('./main');
 module.exports= {
     recipes:recipeData,
     comments: commentsdata,
-    users: userdata
+    users: userdata,
+    main:mainData
 };
 
 
